4369efe7cbff186476c68d64d0e787c96029eec3
Simen Refsland <simen_refsland@hotmail.com>
2024-03-02 10:29:38 +0100

    update header

--- app/page.tsx
+++ app/page.tsx
@@ -76,9 +76,11 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-black text-4xl font-black my-4 ml-4">Connections</h1>
-      <hr className="mb-6 md:mb-12"></hr>
-      <div className="flex flex-col items-center w-11/12 md:w-3/4 lg:w-5/12 mx-auto">
+      <div className="flex flex-col items-center w-11/12 md:w-3/4 lg:w-5/12 mx-auto mt-14">
+        <h1 className="text-black text-4xl font-semibold my-4 ml-4">
+          Connections
+        </h1>
+        <hr className="mb-4 md:mb-4 w-full"></hr>
         <h1 className="text-black mb-4">Create four groups of four!</h1>
         <div className="relative w-full">
           <Popup show={popupState.show} message={popupState.message} />
