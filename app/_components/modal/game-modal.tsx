30253fc9d95c15364a07990fcb173cb953a51f10
Simen Refsland <simen_refsland@hotmail.com>
2024-03-03 21:56:58 +0100

    simplify game modal

--- app/_components/modal/game-modal.tsx
+++ app/_components/modal/game-modal.tsx
@@ -55,12 +55,9 @@ export default function GameModal(props: GameModalProps) {
     <dialog
       ref={dialogRef}
       onClick={handleClickOutside}
-      className={`fixed z-10 inset-0 overflow-y-auto`}
+      className={`backdrop:bg-black backdrop:opacity-75 rounded-md p-8`}
     >
-      <div className="flex items-center justify-center">
-        <div className="fixed inset-0 bg-black opacity-75"></div>
-        <div className="bg-white rounded-lg p-8 z-10">{props.children}</div>
-      </div>
+      {props.children}
     </dialog>
   );
 }
