069cd4392a1461a6c99307ba28fc2070934cc651
Eddy Li <eddylihere@gmail.com>
2024-04-14 21:30:30 -0700

    added second puzzle

---
+++ app/_examples2.ts
@@ -0,0 +1,24 @@
+import { Category } from "./_types";
+
+export const categories: Category[] = [
+  {
+    category: "SQUARE NUMBERS",
+    items: ["36", "49", "64", "81"],
+    level: 1,
+  },
+  {
+    category: "PRIME NUMBERS",
+    items: ["2", "8", "10", "14"],
+    level: 2,
+  },
+  {
+    category: "FIBONACCI NUMBERS",
+    items: ["34", "55", "89", "144"],
+    level: 3,
+  },
+  {
+    category: "PRIME NUMBERS",
+    items: ["5", "14", "42", "132"],
+    level: 4,
+  },
+];
