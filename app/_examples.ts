649beab81ef623d6aa3a3b00c0edfe42763c8837
1434-gauss <152037816+1434-gauss@users.noreply.github.com>
2024-04-24 14:54:55 -0700

    updated puzzles + ordering

--- app/_examples.ts
+++ app/_examples.ts
@@ -1,6 +1,51 @@
 import { Category } from "./_types";
 
-export const categoriesList: Category[][] = [[
+export const categoriesList: Category[][] = [ [
+  {
+    category: "NUMBERS READING THE SAME UPSIDE DOWN",
+    items: ["11", "69", "96", "888"],
+    level: 1,
+  },
+  {
+    category: "CENTRAL BINOMIAL COEFFICIENTS",
+    items: ["20", "70", "252", "924"],
+    level: 4,
+  },
+  {
+    category: "LEAST COMMON MULTIPLE OF FIRST N NUMBERS",
+    items: ["12", "60", "420", "840"],
+    level: 3,
+  },
+  {
+    category: "FIRST DIGITS OF FAMOUS CONSTANTS",
+    items: ["141", "173", "271", "314"],
+    level: 2,
+  }
+],
+                                             [
+  {
+    category: "SQUARE NUMBERS",
+    items: ["36", "49", "64", "81"],
+    level: 1,
+  },
+  {
+    category: "PRIME NUMBERS",
+    items: ["41", "43", "47", "53"],
+    level: 2,
+  },
+  {
+    category: "FIBONACCI NUMBERS",
+    items: ["34", "55", "89", "144"],
+    level: 3,
+  },
+  {
+    category: "CATALAN NUMBERS",
+    items: ["5", "14", "42", "132"],
+    level: 4,
+  },
+],
+                                             
+                                             [
   {
     category: "SQUARE NUMBERS",
     items: ["1", "4", "9", "16"],
@@ -25,27 +70,6 @@ export const categoriesList: Category[][] = [[
 [
   {
     category: "SQUARE NUMBERS",
-    items: ["36", "49", "64", "81"],
-    level: 1,
-  },
-  {
-    category: "PRIME NUMBERS",
-    items: ["41", "43", "47", "53"],
-    level: 2,
-  },
-  {
-    category: "FIBONACCI NUMBERS",
-    items: ["34", "55", "89", "144"],
-    level: 3,
-  },
-  {
-    category: "CATALAN NUMBERS",
-    items: ["5", "14", "42", "132"],
-    level: 4,
-  },
-],[
-  {
-    category: "SQUARE NUMBERS",
     items: ["100", "10000", "4", "1"],
     level: 1,
   },
@@ -65,28 +89,7 @@ export const categoriesList: Category[][] = [[
     level: 4,
   },
 ],
-                                             [
-  {
-    category: "NUMBERS READING THE SAME UPSIDE DOWN",
-    items: ["11", "69", "96", "888"],
-    level: 1,
-  },
-  {
-    category: "CENTRAL BINOMIAL COEFFICIENTS",
-    items: ["20", "70", "252", "924"],
-    level: 4,
-  },
-  {
-    category: "LEAST COMMON MULTIPLE OF FIRST N NUMBERS",
-    items: ["12", "60", "420", "840"],
-    level: 3,
-  },
-  {
-    category: "FIRST DIGITS OF FAMOUS CONSTANTS",
-    items: ["164", "271", "314", "577"],
-    level: 2,
-  }
-]
+                                            
                                             
                                             
                                             ];
